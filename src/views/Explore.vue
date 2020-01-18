e62d56a28e534fa5f210ed7917b8f8e0a518d063
RRapids <tengfeiqaq@gmail.com>
2020-01-18 14:36:08 +0800

    圆桌部分

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,5 +1,7 @@
 <template>
 	<div>
+		<!-- //专题部分 -->
+		<h2 class="title">最新专题</h2>
 		<div class="row">
 			<div class="column" v-for="(item,index) in specials" :key="index">
 				<div class="card">
@@ -13,10 +15,24 @@
 				</div>
 			</div>
 		</div>
+		<!-- //“所有专题” -->
 		<div class="footer">
-			<h3>
-				<router-link to="/special/all" style="color: white;">所有专题--></router-link>
-			</h3>
+			<router-link to="/special/all" class="item">查看更多专题 ></router-link>
+		</div>
+		<!-- //圆桌部分 -->
+		<h2 class="title">圆桌讨论</h2>
+		<div class="row">
+			<div class="column" v-for="(item,index) in roundTables" :key="index">
+				<div class="card">
+					<img :src="item.banner">
+					<h3>{{item.name}}</h3>
+					<p class="meta">{{item.includeCount}}位嘉宾参与</p>
+				</div>
+			</div>
+		</div>
+		<!-- //“所有圆桌” -->
+		<div class="footer">
+			<router-link to="/roundTable/all" class="item">查看更多圆桌 ></router-link>
 		</div>
 	</div>
 </template>
@@ -26,13 +42,20 @@
 		name: 'explore',
 		data() {
 			return {
-				specials: []
+				specials: [],
+				roundTables: []
 			};
 		},
 		created() {
+			//获取最近专题
 			this.axios.get('http://localhost:8080/api/special').then(res => {
 				console.log(res.data.data.length);
 				this.specials = res.data.data;
+			});
+			//获取四个圆桌
+			this.axios.get('http://localhost:8080/api/roundTable').then(res => {
+				console.log(res.data.data.length);
+				this.roundTables = res.data.data;
 			});
 		}
 	};
@@ -55,22 +78,36 @@
 			overflow: auto;
 			border-radius: 10px;
 			break-inside: avoid;
+
 			img {
-				width: 100%;
-				height: 50%;
+				width:500px;
+				height:200px;
 				border-radius: 10px;
 			}
 		}
 	}
 
 	.footer {
-		background-color: blue;
-		padding: 10px;
-		width: 50%;
+		background-color: white;
+		width: 150px;
 		text-align: center;
 		margin-top: 20px;
 		margin: 0 auto;
-		border-radius: 10px;
+		border-radius: 30px;
+		padding: 5px;
+	}
+
+	.item {
+		display: block;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+		font-weight: bold;
+		color: rgb(26, 218, 207);
+	}
+
+	.title {
+		margin-left: 10%;
 	}
 
 	.meta {
